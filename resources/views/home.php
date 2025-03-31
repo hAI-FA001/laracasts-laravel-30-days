03206d4be3ebc3dde794786ce5d643addbe7d787
HaiderAbbas <114867851+hAI-FA001@users.noreply.github.com>
2025-04-01 02:14:31 +0500

    simplify

--- resources/views/home.php
+++ resources/views/home.php
@@ -4,11 +4,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Contact Page</title>
+    <title>Home Page</title>
 </head>
 
 <body>
-    <h1>Hello from Contact Page</h1>
+    <h1>Hello from Home Page</h1>
 </body>
 
 </html>
