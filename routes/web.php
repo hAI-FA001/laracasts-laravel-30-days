a16e5a3fc9e2a2070a0304a97b8aad39db5bc0f1
HaiderAbbas <114867851+hAI-FA001@users.noreply.github.com>
2025-04-01 06:28:55 +0500

    reduce $jobs duplication 1: move to outer scope

--- routes/web.php
+++ routes/web.php
@@ -3,13 +3,7 @@
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('home');
-});
-
-Route::get('/jobs', function () {
-    return view('jobs', [
-        'jobs' => [
+$jobs = [
             [
                 'id' => 1,
                 'title' => 'Director',
@@ -25,31 +19,22 @@ Route::get('/jobs', function () {
                 'title' => 'Teacher',
                 'salary' => '$40,000',
             ],
-        ],
+        ];
+
+Route::get('/', function () {
+    return view('home');
+});
+
+Route::get('/jobs', function () use($jobs) {
+    return view('jobs', [
+        'jobs' => $jobs,
     ]);
 });
 
-Route::get('/jobs/{id}', function ($id) {
+Route::get('/jobs/{id}', function ($id) use($jobs) {
     // dump($id);
     // dd($id);  // "dump and die"
-    $jobs = [
-        [
-            'id' => 1,
-            'title' => 'Director',
-            'salary' => '$50,000',
-        ],
-        [
-            'id' => 2,
-            'title' => 'Programmer',
-            'salary' => '$10,000',
-        ],
-        [
-            'id' => 3,
-            'title' => 'Teacher',
-            'salary' => '$40,000',
-        ],
-    ];
-
+    
     // either add use($id) after function($job) to access $id
     // or a short closure:
     $job = Arr::first($jobs, fn($job) => $job['id'] == $id);
