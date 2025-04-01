15b1a6ed525597164163a5a785a37ccb4a3fbb0a
HaiderAbbas <114867851+hAI-FA001@users.noreply.github.com>
2025-04-02 02:09:07 +0500

    try on /jobs, artisan tinker, mass assigning attributes error (protect from malicious assignment of values), $fillable property

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -11,4 +11,5 @@ class Job extends Model
 {
     // 2 options: rename class to JobListing or do this
     protected $table = 'job_listings';
+    protected $fillable = ['title', 'salary'];  // allow mass assignment on these, won't allow user to update id or other attributes
 }
--- routes/web.php
+++ routes/web.php
@@ -4,11 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Models\Job;
 
 Route::get('/', function () {
-    // convention over configuration: assumes we have table called jobs for class called Job, but ours is called job_listings
-    $jobs = Job::all();
-    // dd($jobs);
-    dd($jobs[0]->title);
-    // return view('home');
+    return view('home');
 });
 
 Route::get('/jobs', function () {
