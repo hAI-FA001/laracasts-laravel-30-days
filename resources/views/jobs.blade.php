1ffea8fe0bc0d570ddc9c35b8fde40d785afb95c
HaiderAbbas <114867851+hAI-FA001@users.noreply.github.com>
2025-04-01 05:14:51 +0500

    create dynamic url in href

--- resources/views/jobs.blade.php
+++ resources/views/jobs.blade.php
@@ -1,7 +1,13 @@
 <x-layout>
     <x-slot:heading>Job Listings</x-slot:heading>
 
-    @foreach ($jobs as $job)
-        <li> <strong>{{ $job['title'] }}:</strong> Pays {{ $job['salary'] }} per year.</li>
-    @endforeach
+    <ul>
+        @foreach ($jobs as $job)
+            <li>
+                <a href='/jobs/{{ $job['id'] }}'>
+                    <strong>{{ $job['title'] }}:</strong> Pays {{ $job['salary'] }} per year.
+                </a>
+            </li>
+        @endforeach
+    </ul>
 </x-layout>
