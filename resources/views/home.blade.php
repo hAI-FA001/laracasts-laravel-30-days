7713ef0b37d213ee5c32cf725d6525c869694054
HaiderAbbas <114867851+hAI-FA001@users.noreply.github.com>
2025-04-01 05:03:20 +0500

    pass data to view, @foreach helper

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,4 +1,8 @@
 <x-layout>
     <x-slot:heading>Home Page</x-slot:heading>
     <h1>Hello from Home Page</h1>
+
+    @foreach ($jobs as $job)
+        <li> <strong>{{ $job['title'] }}:</strong> Pays {{ $job['salary'] }} per year.</li>
+    @endforeach
 </x-layout>
