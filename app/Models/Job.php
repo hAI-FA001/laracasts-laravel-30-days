f66f2050bf8e11081be9520ec43a023615caf44e
HaiderAbbas <114867851+hAI-FA001@users.noreply.github.com>
2025-04-01 23:20:42 +0500

    reduce $jobs duplication 3: models, PSR4 auto-loading

---
+++ app/Models/Job.php
@@ -0,0 +1,29 @@
+<?php
+
+// Laravel uses PSR4 standard for auto-loading, defined in composer.json
+// namespace and its path is defined there
+namespace App\Models;
+
+class Job
+{
+    public static function all(): array
+    {
+        return [
+            [
+                'id' => 1,
+                'title' => 'Director',
+                'salary' => '$50,000',
+            ],
+            [
+                'id' => 2,
+                'title' => 'Programmer',
+                'salary' => '$10,000',
+            ],
+            [
+                'id' => 3,
+                'title' => 'Teacher',
+                'salary' => '$40,000',
+            ],
+        ];
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -2,30 +2,8 @@
 
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
+use App\Models\Job;
 
-class Job
-{
-    public static function all(): array
-    {
-        return [
-            [
-                'id' => 1,
-                'title' => 'Director',
-                'salary' => '$50,000',
-            ],
-            [
-                'id' => 2,
-                'title' => 'Programmer',
-                'salary' => '$10,000',
-            ],
-            [
-                'id' => 3,
-                'title' => 'Teacher',
-                'salary' => '$40,000',
-            ],
-        ];
-    }
-}
 
 Route::get('/', function () {
     return view('home');
