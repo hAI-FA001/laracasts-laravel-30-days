fdb14d800276c8d6e368cc5ed620cea01a549a91
HaiderAbbas <114867851+hAI-FA001@users.noreply.github.com>
2025-04-01 05:15:28 +0500

    create dynamic route handler/route param, leverage dump(), dd(), Arr::first and short closures

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -26,6 +27,35 @@ Route::get('/jobs', function () {
             ],
         ],
     ]);
+});
+
+Route::get('/jobs/{id}', function ($id) {
+    // dump($id);
+    // dd($id);  // "dump and die"
+    $jobs = [
+        [
+            'id' => 1,
+            'title' => 'Director',
+            'salary' => '$50,000',
+        ],
+        [
+            'id' => 2,
+            'title' => 'Programmer',
+            'salary' => '$10,000',
+        ],
+        [
+            'id' => 3,
+            'title' => 'Teacher',
+            'salary' => '$40,000',
+        ],
+    ];
+
+    // either add use($id) after function($job) to access $id
+    // or a short closure:
+    $job = Arr::first($jobs, fn($job) => $job['id'] == $id);
+    // dd($job);
+
+    return view('job', ['job' => $job]);
 });
 
 Route::get('/contact', function () {
