0443dd035a3397f1cbc2051250460764d6c84091
HaiderAbbas <114867851+hAI-FA001@users.noreply.github.com>
2025-04-04 11:56:05 +0500

    re-add all actions

--- routes/web.php
+++ routes/web.php
@@ -4,10 +4,6 @@ use App\Http\Controllers\JobController;
 use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'home');
-
-Route::resource('jobs', JobController::class, [
-    // 'only' => ['index', 'show', 'create', 'store'],
-    // 'except' => ['edit'],
-]);
-
 Route::view('/contact', 'contact');
+
+Route::resource('jobs', JobController::class);
