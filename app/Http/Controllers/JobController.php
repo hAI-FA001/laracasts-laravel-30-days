629ffcae4b46720bfd6e002325b3c282207aa8a5
HaiderAbbas <114867851+hAI-FA001@users.noreply.github.com>
2025-04-04 11:41:03 +0500

    Routes Reloaded, Tip 2: Controller classes, artisan make:controller, turn closures into actions on the controller, cleanup

---
+++ app/Http/Controllers/JobController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Job;
+use Illuminate\Http\Request;
+
+class JobController extends Controller
+{
+    public function index()
+    {
+        $jobs = Job::with('employer')->latest()->cursorPaginate(3);
+
+        return view('jobs.index', [
+            'jobs' => $jobs,
+        ]);
+    }
+
+    public function create()
+    {
+        return view('jobs.create');
+    }
+
+    public function show(Job $job)
+    {
+        return view('jobs.show', ['job' => $job]);
+    }
+
+    public function store()
+    {
+        request()->validate([
+            // ref: https://laravel.com/docs/12.x/validation#available-validation-rules
+            'title' => ['required', 'min:3'],
+            'salary' => ['required'],
+        ]);
+
+        Job::create([
+            'title' => request('title'),
+            'salary' => request('salary'),
+            'employer_id' => 1,
+        ]);
+
+        return redirect('/jobs');
+    }
+
+    public function edit(Job $job)
+    {
+        return view('jobs.edit', [
+            'job' => $job,
+        ]);
+    }
+
+    public function update(Job $job)
+    {
+        request()->validate([
+            'title' => ['required', 'min:3'],
+            'salary' => ['required'],
+        ]);
+
+        // authorize (later)
+
+        $job->update([
+            'title' => request('title'),
+            'salary' => request('salary'),
+        ]);
+
+        return redirect('/jobs/' . $job->id);
+    }
+
+    public function destroy(Job $job)
+    {
+        // authorize (later)
+
+        $job->delete();
+
+        return redirect('/jobs');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,92 +1,19 @@
 <?php
 
+use App\Http\Controllers\JobController;
 use Illuminate\Support\Facades\Route;
-use App\Models\Job;
 
 Route::get('/', function () {
     return view('home');
 });
 
-// Index
-Route::get('/jobs', function () {
-    $jobs = Job::with('employer')->latest()->cursorPaginate(3);
-
-    return view('jobs.index', [
-        'jobs' => $jobs,
-    ]);
-});
-
-// Create
-Route::get('/jobs/create', function () {
-    return view('jobs.create');
-});
-
-// Show
-// Model Binding
-// - wild card and parameter name need to be identical
-// - add a type to the parameter
-// By default, it fetches based on id, but we can change that: Route::get('posts/{post:slug}')
-Route::get('/jobs/{job}', function (Job $job) {
-    return view('jobs.show', ['job' => $job]);
-});
-
-// Store
-Route::post('/jobs', function () {
-    request()->validate([
-        // ref: https://laravel.com/docs/12.x/validation#available-validation-rules
-        'title' => ['required', 'min:3'],
-        'salary' => ['required'],
-    ]);
-
-    Job::create([
-        'title' => request('title'),
-        'salary' => request('salary'),
-        'employer_id' => 1,
-    ]);
-
-    return redirect('/jobs');
-});
-
-// Edit
-Route::get('/jobs/{job}/edit', function (Job $job) {
-    return view('jobs.edit', [
-        'job' => $job,
-    ]);
-});
-
-// Update
-Route::patch('/jobs/{job}', function (Job $job) {
-    // validate
-    request()->validate([
-        'title' => ['required', 'min:3'],
-        'salary' => ['required'],
-    ]);
-
-    // authorize (later)
-
-    // update job
-    // and persist
-    // headstart: don't need to fetch it ourselves, check "Route Model Binding"
-    $job->update([
-        'title' => request('title'),
-        'salary' => request('salary'),
-    ]);
-
-
-    // redirect to job-specific page
-    return redirect('/jobs/' . $job->id);
-});
-
-// Destroy
-Route::delete('/jobs/{job}', function (Job $job) {
-    // authorize (later)
-
-    // delete job
-    $job->delete();  // can inline instead of first storing in $job
-
-    // redirect
-    return redirect('/jobs');
-});
+Route::get('/jobs', [JobController::class, 'index']);
+Route::get('/jobs/create', [JobController::class, 'create']);
+Route::get('/jobs/{job}', [JobController::class, 'show']);
+Route::post('/jobs', [JobController::class, 'store']);
+Route::get('/jobs/{job}/edit', [JobController::class, 'edit']);
+Route::patch('/jobs/{job}', [JobController::class, 'update']);
+Route::delete('/jobs/{job}', [JobController::class, 'destroy']);
 
 Route::get('/contact', function () {
     return view('contact');
