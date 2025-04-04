c52cbcd818cac9cc05da4a6d02f1104e1eb9d845
HaiderAbbas <114867851+hAI-FA001@users.noreply.github.com>
2025-04-04 11:53:03 +0500

    Routes Reloaded, Tip 6.2: "only" and "except" for when you don't want all routes for a resource

--- routes/web.php
+++ routes/web.php
@@ -5,18 +5,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'home');
 
-// Route::controller(JobController::class)->group(function () {
-//     Route::get('/jobs', 'index');
-//     Route::get('/jobs/create', 'create');
-//     Route::get('/jobs/{job}', 'show');
-//     Route::post('/jobs', 'store');
-//     Route::get('/jobs/{job}/edit', 'edit');
-//     Route::patch('/jobs/{job}', 'update');
-//     Route::delete('/jobs/{job}', 'destroy');
-// });
-
-// first arg: resource name
-// second arg: controller responsible for it
-Route::resource('jobs', JobController::class);
+Route::resource('jobs', JobController::class, [
+    // 'only' => ['index', 'show', 'create', 'store'],
+    // 'except' => ['edit'],
+]);
 
 Route::view('/contact', 'contact');
