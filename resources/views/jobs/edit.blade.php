a7be3a0668585e809045e60fc477452f9236a230
HaiderAbbas <114867851+hAI-FA001@users.noreply.github.com>
2025-04-04 11:19:30 +0500

    add delete button + style area with Cancel and Update

--- resources/views/jobs/edit.blade.php
+++ resources/views/jobs/edit.blade.php
@@ -15,9 +15,7 @@
                                 class="flex items-center rounded-md bg-white pl-3 outline-1 -outline-offset-1 outline-gray-300 focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-indigo-600">
                                 <input type="text" name="title" id="title"
                                     class="block min-w-0 grow py-1.5 px-3 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6"
-                                    placeholder="Shift Leader" required
-                                    value="{{ $job->title }}"
-                                    >
+                                    placeholder="Shift Leader" required value="{{ $job->title }}">
                             </div>
 
                             @error('title')
@@ -33,9 +31,7 @@
                                 class="flex items-center rounded-md bg-white pl-3 outline-1 -outline-offset-1 outline-gray-300 focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-indigo-600">
                                 <input type="text" name="salary" id="salary"
                                     class="block min-w-0 grow py-1.5 px-3 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6"
-                                    placeholder="$50,000 Per Year" required
-                                    value="{{ $job->salary }}"
-                                    >
+                                    placeholder="$50,000 Per Year" required value="{{ $job->salary }}">
                             </div>
 
                             @error('salary')
@@ -47,12 +43,22 @@
             </div>
         </div>
 
-        <div class="mt-6 flex items-center justify-end gap-x-6">
-            <a href="/jobs/{{ $job->id }}" class="text-sm/6 font-semibold text-gray-900">Cancel</a>
-            <button type="submit"
-                class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
-                Update
-            </button>
+        <div class="mt-6 flex items-center justify-between gap-x-6">
+            <div class="flex items-center">
+                <button class="text-red-500 text-sm font-bold">Delete</button>
+            </div>
+
+            <div class="flex items-center gap-x-6">
+                <a href="/jobs/{{ $job->id }}" class="text-sm/6 font-semibold text-gray-900">Cancel</a>
+
+                {{-- wrap <button> in a <div> to avoid any weird issues --}}
+                <div>
+                    <button type="submit"
+                        class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+                        Update
+                    </button>
+                </div>
+            </div>
         </div>
     </form>
 
