06e161685c8b9f9a6661df6c72e9ebfeff2a27b5
HaiderAbbas <114867851+hAI-FA001@users.noreply.github.com>
2025-04-07 10:19:40 +0500

    add login logic: Auth::attempt, ValidationException, regenerate session

--- app/Http/Controllers/SessionController.php
+++ app/Http/Controllers/SessionController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\Rules\Password;
+use Illuminate\Validation\ValidationException;
 
 class SessionController extends Controller
 {
@@ -14,7 +16,24 @@ class SessionController extends Controller
 
     public function store()
     {
-        dd(request()->all());
+        // validate
+        $validatedAttributes = request()->validate([
+            'email' => ['required', 'email'],
+            'password' => ['required'],
+        ]);
+
+        // attempt to log in
+        if (!Auth::attempt($validatedAttributes)) {
+            throw ValidationException::withMessages([
+                'email' => 'Sorry, those credentials don\'t match',
+            ]);
+        }
+
+        // regenerate the session token
+        request()->session()->regenerate();
+
+        // redirect
+        return redirect('/jobs');
     }
 
     public function destroy()
