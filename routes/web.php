03c569bde5efe6945a479ae61c23389fb8999ec7
HaiderAbbas <114867851+hAI-FA001@users.noreply.github.com>
2025-04-07 09:23:57 +0500

    add route and controller for /register

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,13 @@
 <?php
 
 use App\Http\Controllers\JobController;
+use App\Http\Controllers\RegisterUserController;
 use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'home');
 Route::view('/contact', 'contact');
 
 Route::resource('jobs', JobController::class);
+
+// Auth
+Route::get('/register', [RegisterUserController::class, 'create']);
