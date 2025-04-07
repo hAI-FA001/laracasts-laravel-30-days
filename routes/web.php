7dcae06bbf1811e7c3864d8b842d3021572e3aad
HaiderAbbas <114867851+hAI-FA001@users.noreply.github.com>
2025-04-07 09:34:06 +0500

    add /login routes and SessionController

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\JobController;
 use App\Http\Controllers\RegisterUserController;
+use App\Http\Controllers\SessionController;
 use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'home');
@@ -12,3 +13,6 @@ Route::resource('jobs', JobController::class);
 // Auth
 Route::get('/register', [RegisterUserController::class, 'create']);
 Route::post('/register', [RegisterUserController::class, 'store']);
+
+Route::get('/login', [SessionController::class, 'create']);
+Route::post('/login', [SessionController::class, 'store']);
