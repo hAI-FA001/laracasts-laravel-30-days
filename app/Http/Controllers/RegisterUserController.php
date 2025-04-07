23b0f51f5af2cad75eadd7b2e5c18037ec3dcc33
HaiderAbbas <114867851+hAI-FA001@users.noreply.github.com>
2025-04-07 10:01:28 +0500

    register user logic: Password validation rules class, validatedAttributes, Auth facade for logging in

--- app/Http/Controllers/RegisterUserController.php
+++ app/Http/Controllers/RegisterUserController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\Rules\Password;
 
 class RegisterUserController extends Controller
 {
@@ -13,6 +15,23 @@ class RegisterUserController extends Controller
 
     public function store()
     {
-        dd(request()->all());
+        // validate
+        $validatedAttributes = request()->validate([
+            'first_name' => ['required'],
+            'last_name' => ['required'],
+            'email' => ['required', 'email'],
+            // check docs, can set these on service provider so we can can do Password:defaults()
+            // "confirmed" makes sure attribute and attribute_confirmation match, this is why we set the name HTML attribute to "password_confirmation" and not "confirm_password"
+            'password' => ['required', Password::min(5)->letters()->numbers(), 'confirmed'],
+        ]);
+
+        // create and persist in DB
+        $user = User::create($validatedAttributes); // shortcut, validate() returns the validated attributes
+
+        // log in
+        Auth::login($user);
+
+        // redirect
+        return redirect('/jobs');
     }
 }
