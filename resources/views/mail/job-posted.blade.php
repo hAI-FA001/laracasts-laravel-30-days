49a9cb66b47b4796d9ae804ff989dacb19fd82cb
HaiderAbbas <114867851+hAI-FA001@users.noreply.github.com>
2025-04-11 12:07:06 +0500

    update view, use $job, creating full url with url()

--- resources/views/mail/job-posted.blade.php
+++ resources/views/mail/job-posted.blade.php
@@ -1,1 +1,12 @@
-Congrats! Your job is now live on our website.
+<h2>
+    {{ $job->title }}
+</h2>
+
+<p>
+    Congrats! Your job is now live on our website.
+</p>
+
+<p>
+    {{-- user won't be in our website, so we can't just do /jobs/etc --}}
+    <a href="{{ url('/jobs/' . $job->id) }}">View Your Job Listing</a>
+</p>
