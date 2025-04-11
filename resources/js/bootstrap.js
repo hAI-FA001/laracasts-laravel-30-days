a0b091648d5adc44176acec094c672550d00d240
HaiderAbbas <114867851+hAI-FA001@users.noreply.github.com>
2025-04-11 16:53:49 +0500

    instant reloading (making changes w/o losing application state), editing app.js

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,4 +1,7 @@
-import axios from 'axios';
+// this file bootstraps the frontend dependencies
+// in this case, hx-request are common to make so Laravel sets up initial config
+
+import axios from "axios";
 window.axios = axios;
 
-window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+window.axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
