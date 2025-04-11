3db398d74d03c46f8e6287f671ac87d35710d567
HaiderAbbas <114867851+hAI-FA001@users.noreply.github.com>
2025-04-11 16:48:21 +0500

    Vite, modifying app.css in resources, npm run dev to run vite, @vite to include app.css

--- resources/views/Components/layout.blade.php
+++ resources/views/Components/layout.blade.php
@@ -6,6 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home Page</title>
 
+    @vite(['resources/css/app.css'])
+
     <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
 </head>
 
--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,15 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import tailwindcss from '@tailwindcss/vite';
+// Laravel suggests axios and vite as seen in package.json
+// run "npm install" to get these
+// then configure vite from here
+
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ["resources/css/app.css", "resources/js/app.js"],
             refresh: true,
         }),
         tailwindcss(),
